3dfe920761e5d26aaeb1870fab91fdd56a23e854
Miloš Ostojić <ostojic.milos@gmail.com>
2019-03-11 16:54:13 +0100

    Default generator command stub

--- Ultramarine.VSExtension/GeneratorsPackage.cs
+++ Ultramarine.VSExtension/GeneratorsPackage.cs
@@ -36,6 +36,7 @@ namespace Ultramarine.VSExtension
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [Guid(GeneratorsPackage.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class GeneratorsPackage : AsyncPackage
     {
         /// <summary>
@@ -68,6 +69,7 @@ namespace Ultramarine.VSExtension
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await Ultramarine.VSExtension.GeneratorCommand.InitializeAsync(this);
         }
 
         #endregion
