74d8e0d03829eb12fc48266c36eb2a3f72fe750c
Miloš Ostojić <ostojic.milos@gmail.com>
2019-03-15 21:42:52 +0100

    querylanguage cli - stub for vs code

---
+++ Ultramarine.QueryLanguage.Cli/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ultramarine.QueryLanguage.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.OutputEncoding = System.Text.Encoding.GetEncoding(28591);
+            Console.WriteLine(Logo.Art);
+            Console.WriteLine("Ultramarine QueryLanguage CLI");
+            Console.ResetColor();
+            
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+                var compiler = new ConditionCompiler(input);
+                Console.WriteLine(compiler.Execute());
+            }
+
+        }
+    }
+
+}
