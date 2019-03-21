6f5d9368283e5ab458a3dcea6a75a72ec82def2e
Bozidar Trifunovic <bozidar.trifunovic85@gmail.com>
2019-03-21 14:53:46 +0100

    extensions for vs code

--- Ultramarine.VSCodeExtension/extension.js
+++ Ultramarine.VSCodeExtension/extension.js
@@ -1,6 +1,7 @@
+const path = require("path");
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,22 +10,49 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "helloworldcomponent" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ultramarine-vscodeextension" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "extension.helloWorld",
+    function() {
+      // The code you place here will be executed every time your command is executed
+      const panel = vscode.window.createWebviewPanel(
+        "reactComponent", // Identifies the type of the webview. Used internally
+        "React component", // Title of the panel displayed to the user
+        vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+        {
+          enableScripts: true
+        }
+      );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      const manifest = require(path.join(
+        context.extensionPath,
+        "build",
+        "asset-manifest.json"
+      ));
+      const mainScript = manifest["main.js"];
+      // const mainStyle = manifest["main.css"];
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+      const scriptPathOnDisk = vscode.Uri.file(
+        path.join(context.extensionPath, "build", mainScript)
+      );
+      const scriptUri = scriptPathOnDisk.with({ scheme: "vscode-resource" });
 
-	context.subscriptions.push(disposable);
+      panel.webview.html = getWebviewContent(scriptUri);
+      console.log(panel.webview.html);
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World!");
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
@@ -32,6 +60,35 @@ exports.activate = activate;
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate
+};
+
+function getWebviewContent(scriptUri) {
+  //const nonce = getNonce();
+  return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+				<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+				<meta name="theme-color" content="#000000">
+				<title>React App Boza</title>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+    <p>boza</p>
+    <script src="${scriptUri}" ></script>
+  </body>
+</html>`;
 }
+
+// function getNonce() {
+//   let text = "";
+//   const possible =
+//     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//   for (let i = 0; i < 32; i++) {
+//     text += possible.charAt(Math.floor(Math.random() * possible.length));
+//   }
+//   return text;
+// }
---
+++ Ultramarine.VSCodeExtension/src/components/Greetings.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Greeting extends React.Component {
+  render() {
+    return <p>Hello world Bozidar</p>;
+  }
+}
+
+export default Greeting;
