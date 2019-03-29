06eb4dcca8293762c63251760fde97078acfd326
Miloš Ostojić <ostojic.milos@gmail.com>
2019-03-29 15:50:28 +0100

    simple LoadProjectItem task

---
+++ Ultramarine.Generators.Tasks/LoadProjectItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Text;
+using Ultramarine.Generators.Tasks.Library.Contracts;
+using Ultramarine.Workspaces;
+
+namespace Ultramarine.Generators.Tasks
+{
+    [Export(typeof(Task))]
+    public class LoadProjectItem : Task
+    {
+        public string ProjectName { get; set; }
+        public string ItemName { get; set; }
+        public string LinkedWith { get; set; }
+
+        protected override object OnExecute()
+        {
+            var project = ExecutionContext.GetProject(ProjectName);
+            if (project == null)
+                throw new ArgumentException($"There is no project named {ProjectName}");
+
+            var item = project.FindProjectItem(ItemName);
+            if (item == null)
+                throw new ArgumentException($"There is no project item named {ItemName} in project {ProjectName}");
+            return item;
+        }
+    }
+}
