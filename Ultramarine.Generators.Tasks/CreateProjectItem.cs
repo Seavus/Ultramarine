77212da83bc1db2a3c299bff5b6a860038a349e6
Miloš Ostojić <ostojic.milos@gmail.com>
2019-04-16 12:18:08 +0200

    create project item now supports specifying custom project to create an item into

--- Ultramarine.Generators.Tasks/CreateProjectItem.cs
+++ Ultramarine.Generators.Tasks/CreateProjectItem.cs
@@ -12,15 +12,16 @@ namespace Ultramarine.Generators.Tasks
         public string ItemName { get; set; }
         public string FolderPath { get; set; }
         public string LinkedWith { get; set; }
+        public string ProjectName { get; set; }
         public bool Overwrite { get; set; }
 
         protected override object OnExecute()
         {
-            var projectItemPath = ExecutionContext.FilePath;
+            var projectItemPath = string.IsNullOrWhiteSpace(ProjectName) ? ExecutionContext.FilePath : ProjectName;
             var folderPath = FolderPath ?? string.Empty;
             projectItemPath = Path.Combine(projectItemPath, folderPath, ItemName);
 
             return ExecutionContext.CreateProjectItem(projectItemPath, Input as string, Overwrite);
-        }        
+        }
     }
 }
