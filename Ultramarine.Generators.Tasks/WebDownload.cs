dcbc929dc1c570b5baa416c96ab39c6572e50e9f
Aleksa <aleksavasiljevic07@gmail.com>
2019-04-22 10:08:30 +0200

    Webdownload task

---
+++ Ultramarine.Generators.Tasks/WebDownload.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.IO;
+using System.Net;
+using System.Text;
+using Ultramarine.Generators.Tasks.Library.Contracts;
+
+namespace Ultramarine.Generators.Tasks
+{
+    [Export(typeof(Task))]
+    public class WebDownload : Task
+    {
+        public string DownloadLink { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool UseSSL { get; set; }
+
+        protected override object OnExecute()
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.Headers.Add("user-agent", "Mozilla/4.0 (Compatible; Windows NT 5.1; MSIE 6.0)");
+
+                    if (!string.IsNullOrEmpty(Password) || !string.IsNullOrEmpty(Username))
+                    {
+                        client.UseDefaultCredentials = true;
+                        client.Credentials = new NetworkCredential(Username, Password);
+                    }
+
+                    if (UseSSL)
+                        ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+
+                    MemoryStream stream;
+                    stream = new MemoryStream(client.DownloadData(DownloadLink));
+                    
+                        return stream;
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Ultramarine.Generators.Tests/WebDownloadTestscs.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ultramarine.Generators.Serialization.Providers;
+using Ultramarine.Generators.Tasks;
+using Ultramarine.Workspaces;
+
+namespace Ultramarine.Generators.Tests
+{
+    [TestClass]
+    public class WebDownloadTests
+    {
+        [TestMethod]
+        [DeploymentItem("Samples\\WebDownload\\WebDownload.gen.json", "Samples")]
+        public void ShouldDeserializeGeneratorConfig()
+        {
+            var generatorPath = "Samples\\WebDownload\\WebDownload.gen.json";
+            var generator = GeneratorSerializer.Instance.Load(generatorPath);
+
+            Assert.IsNotNull(generator);
+            Assert.AreEqual(generator.Name, "generator1");
+            Assert.AreEqual(generator.Description, "generator1description");
+        }
+
+        [TestMethod]
+        [DeploymentItem("Samples\\WebDownload\\WebDownload.gen.json", "Samples")]
+        public void ShouldDeserializeTaskCollection()
+        {
+            var generatorPath = "Samples\\WebDownload\\WebDownload.gen.json";
+            var generator = GeneratorSerializer.Instance.Load(generatorPath);
+            Assert.IsNotNull(generator.Tasks);
+        }
+
+        [TestMethod]
+        [DeploymentItem("Samples\\WebDownload\\WebDownload.gen.json", "Samples")]
+        public void TaskCollectionShouldHaveCreateFolderTest()
+        {
+            var generatorPath = "Samples\\WebDownload\\WebDownload.gen.json";
+            var generator = GeneratorSerializer.Instance.Load(generatorPath);
+            Assert.IsNotNull(generator.Tasks.FirstOrDefault());
+            var SqlCommandTask = generator.Tasks.First();
+            Assert.IsInstanceOfType(SqlCommandTask, typeof(WebDownload));
+            Assert.AreEqual(SqlCommandTask.Name, "WebDownloadTask");
+            Assert.AreEqual(SqlCommandTask.Description, "WebDownloadTaskDescription");
+        }
+
+        [TestMethod]
+        [DeploymentItem("Samples\\WebDownload\\WebDownload.gen.json", "Samples")]
+        public void ShouldDownloadFile()
+        {
+            var generatorPath = "Samples\\WebDownload\\WebDownload.gen.json";
+            var generator = GeneratorSerializer.Instance.Load(generatorPath);
+            var loggerMock = new Mock<ILogger>();
+
+            generator.SetLogger(loggerMock.Object);
+            generator.Execute();
+
+            var task = generator.Tasks.First();
+
+            Assert.AreEqual(task.Output, 1); //temporary (num of rows)
+        }
+    }
+}
