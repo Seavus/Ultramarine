ae4cbe3231ad36d73409001432ddf53ee3143c3a
Miloš Ostojić <ostojic.milos@gmail.com>
2019-04-23 13:43:02 +0200

    LoadCodeElement - support for base types

--- Ultramarine.Generators.Tasks/LoadCodeElement.cs
+++ Ultramarine.Generators.Tasks/LoadCodeElement.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Composition;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using Ultramarine.Generators.Tasks.Library.Contracts;
 using Ultramarine.Workspaces;
 using Ultramarine.Workspaces.CodeElements;
@@ -17,7 +18,7 @@ namespace Ultramarine.Generators.Tasks
         public ElementType? ElementType { get; set; }
         public ElementAccess? ElementAccess { get; set; }
         public ElementOverride? ElementOverride { get; set; }
-        public string InstanceOf { get; set; }
+        public string TypeOf { get; set; }
 
         protected override object OnExecute()
         {
@@ -25,23 +26,23 @@ namespace Ultramarine.Generators.Tasks
             var input = Input as IEnumerable;
             if (input == null) return result;
 
-            foreach(var item in input)
+            foreach (var item in input)
             {
                 if (item is IProjectItemModel)
                 {
                     var elements = ((IProjectItemModel)item).GetCodeElements(ElementName);
-                    if(elements != null)
-                    result.AddRange(elements);
+                    if (elements != null)
+                        result.AddRange(elements);
                 }
                 //TODO: add any other type of inputs
             }
 
-            result = FilterElements(result, ElementType, ElementAccess, ElementOverride, InstanceOf);
+            result = FilterElements(result, ElementType, ElementAccess, ElementOverride, TypeOf);
 
             return result;
         }
 
-        private List<ICodeElementModel> FilterElements(List<ICodeElementModel> elements, ElementType? type, ElementAccess? access, ElementOverride? @override, string instanceOf = null)
+        private List<ICodeElementModel> FilterElements(List<ICodeElementModel> elements, ElementType? type, ElementAccess? access, ElementOverride? @override, string typeOf = null)
         {
             var result = elements;
             if (type.HasValue)
@@ -50,8 +51,9 @@ namespace Ultramarine.Generators.Tasks
                 result = result.Where(e => e.Access == access).ToList();
             if (@override.HasValue)
                 result = result.Where(e => e.Override == @override).ToList();
+            if (typeOf != null)
+                result = result.Where(e => e.TypeOf != null && e.TypeOf.Any(t => Regex.IsMatch(t, typeOf))).ToList();
 
-            //TODO: rethink instanceOf
             return result;
         }
     }
