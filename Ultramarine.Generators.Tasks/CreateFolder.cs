08b6b9238bcf670a12c13acb8eebc647fe1661a9
Miloš Ostojić <ostojic.milos@gmail.com>
2019-05-13 13:15:58 +0200

    CreateFolder now supports global and local variables

--- Ultramarine.Generators.Tasks/CreateFolder.cs
+++ Ultramarine.Generators.Tasks/CreateFolder.cs
@@ -1,18 +1,44 @@
-﻿using System;
-using System.Composition;
+﻿using System.Composition;
 using System.IO;
 using Ultramarine.Generators.Tasks.Library.Contracts;
 
 namespace Ultramarine.Generators.Tasks
 {
+    /// <summary>
+    /// Creates a folder on a specified location
+    /// </summary>
     [Export(typeof(Task))]
     public class CreateFolder : Task
     {
-        public string FolderPath { get; set; }
-        public DirectoryInfo BasePath => Directory.GetParent(ExecutionContext.FilePath);
+        private string _folderPath;
+        private string _basePath;
+
+        /// <summary>
+        /// Folder path to create
+        /// <para>This property supports Variables</para>
+        /// </summary>
+        public string FolderPath { get => TryGetSettingValue(_folderPath) as string; set => _folderPath = value; }
+        /// <summary>
+        /// Base path of the folder to create
+        /// <para>This property supports Variables</para>
+        /// </summary>
+        public string BasePath { get => TryGetSettingValue(_basePath) as string; set => _basePath = value; }
+
         protected override object OnExecute()
         {
+            if(string.IsNullOrWhiteSpace(BasePath))
             return ExecutionContext.CreateDirectory(FolderPath);
+
+            var directory = new DirectoryInfo(Path.Combine(BasePath, FolderPath));
+            directory.Create();
+            return directory.ToString();
+        }
+
+        protected override ValidationResult Validate()
+        {
+            if (string.IsNullOrWhiteSpace(FolderPath))
+                return new ValidationResult(nameof(FolderPath), "FolderPath must be specified.");
+            return base.Validate();
         }
     }
 }
