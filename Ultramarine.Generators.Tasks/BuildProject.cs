c69540b1a94baef73b5bee5873b844d72968c559
Miloš Ostojić <ostojic.milos@gmail.com>
2019-05-13 13:04:13 +0200

    BuildProject now supports global and local variables

--- Ultramarine.Generators.Tasks/BuildProject.cs
+++ Ultramarine.Generators.Tasks/BuildProject.cs
@@ -5,19 +5,34 @@ using Ultramarine.Workspaces;
 
 namespace Ultramarine.Generators.Tasks
 {
+    /// <summary>
+    /// Project build task
+    /// </summary>
     [Export(typeof(Task))]
     public class BuildProject : Task
     {
-        public string ProjectName { get; set; }
-        public string Configuration { get; set; }
+        private string _projectName;
+        private string _configuration = "Debug";
+        /// <summary>
+        /// Name of the project to build
+        /// <para>This property supports Variables and QueryLanguage Conditions</para>
+        /// <c>If left empty, the current execution context is being used (a project containing configuration file)</c>
+        /// </summary>
+        public string ProjectName { get => TryGetSettingValue(_projectName) as string; set => _projectName = value; }
+        /// <summary>
+        /// Name of the configuration to build
+        /// <para>This property supports Variables</para>
+        /// <c>Defaults to Debug</c>
+        /// </summary>
+        public string Configuration { get => TryGetSettingValue(_configuration) as string; set => _configuration = value; }
 
         protected override object OnExecute()
         {
             var result = new Dictionary<string, bool>();
             var projects = string.IsNullOrWhiteSpace(ProjectName)
                 ? new List<IProjectModel> { ExecutionContext }
-                : ExecutionContext.GetProjects(ProjectName);
-            foreach(var project in projects)
+                : ExecutionContext.GetProjects(ProjectName);            
+            foreach (var project in projects)
             {
                 var buildResult = project.Build(Configuration);
                 result.Add(project.Name, buildResult);
